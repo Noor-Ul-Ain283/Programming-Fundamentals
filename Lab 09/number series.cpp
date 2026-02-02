d6bc5b86d90d94df6499731efbcd2555b40a3b25
Noor-Ul-Ain283 <nooraineabbasi@gmail.com>
2026-02-02 21:17:31 +0500

    Lab folder with proper sequence

---
+++ Lab 09/number series.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i=1, n;
+	cout<<"Enter limit: ";
+	cin>>n;
+	//using while loop
+	while(i<=n)
+	{
+		cout<<i<<endl;
+		i++;
+	}
+	
+	
+	return 0;
+}
